d1d6689b3030c697a123f5a698d4be181d6600ea
Dani Santoso <albumkeluargadafa@gmail.com>
2026-05-28 03:36:23 +0700

    fix: export clearPartials from public API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { render, renderStream, compile, clearCache, purgeTemplate, getCacheStats } from './renderer.js'
 export { compileToFunction, type SourceMapEntry } from './compiler.js'
+export { clearPartials } from './runtime.js'
 export * as adapter from './adapters/index.js'
 export type { Token, ASTNode, RenderOptions, CompiledTemplate, ExprNode, Plugin } from './types.js'
 export type { CacheStats } from './cache.js'
